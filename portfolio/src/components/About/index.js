54b749711054630ba8fd53480df69e992f8e9fce
Pranav <pranavkharche8@gmail.com>
2023-11-18 12:22:50 +0530

    first commit

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const SocialMediaIcons = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+
+const SocialMediaIcon = styled.a`
+  display: inline-block;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.text_primary};
+  transition: color 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About;
+
